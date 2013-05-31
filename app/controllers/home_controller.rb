c4f04765d2f57793796f1ed9a850f37776b86bad
ryan <rflynn32@gmail.com>
2013-05-31 15:34:43 -0700

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+class HomeController < ApplicationController
+
+	def index
+
+	end
+
+	def bio
+
+	end
+
+	def html
+
+	end
+
+	def javascript
+
+	end
+
+	def rails
+
+	end
+
+	def contact
+
+	end
+
+	def extra
+
+	end
+
+	def resume
+
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalSite::Application.config.secret_token = 'b0877bdbde5f399f1f61a86c15fe16eff8fd13f4626ce8cecf3ddebacac43d589120c577aeafaa94eabb4caf9b8688e5b376d01448435faf216c111142120226'
